4740d478ee6ffb4881d3a60d1e4a9c59afae1426
keyongjin <34257484+keyongjin@users.noreply.github.com>
2018-06-01 11:20:51 +0800

    deleteSVN.bat
    删除。SVN文件

---
+++ deleteSVN.bat
@@ -0,0 +1,8 @@
+@echo ON
+@Rem 删除SVN版本控制目录
+
+@for /r . %%a in (.) do @if exist "%%a\.svn" rd /s /q "%%a\.svn"
+@Rem for /r . %%a in (.) do @if exist "%%a\.svn" @echo "%%a\.svn"
+
+@echo Completed.
+@pause
